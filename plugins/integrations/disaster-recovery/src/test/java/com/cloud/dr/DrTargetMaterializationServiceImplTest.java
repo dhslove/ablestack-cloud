19c99b75295a0379062d3265ff30ee7dba87a31c
DH Park <pdh1974@gmail.com>
2026-08-28 14:23:01 +0900

    Revert "test: align DR cleanup stubs"
    This reverts commit b3c9fa78bf11dc4f3b56d030a2c3bab953281bf4.

--- plugins/integrations/disaster-recovery/src/test/java/com/cloud/dr/DrTargetMaterializationServiceImplTest.java
+++ plugins/integrations/disaster-recovery/src/test/java/com/cloud/dr/DrTargetMaterializationServiceImplTest.java
@@ -158,6 +158,7 @@ public class DrTargetMaterializationServiceImplTest {
         Mockito.when(drPlanDao.findById(planId)).thenReturn(plan);
         Mockito.when(accountDao.findById(1L)).thenReturn(owner);
         Mockito.when(disk.getId()).thenReturn(2L);
+        Mockito.when(disk.getProvider()).thenReturn("RBD");
         Mockito.when(disk.getArtifactRef()).thenReturn("rbd:rbd/test-clone");
         Mockito.when(disk.getTargetVolumeId()).thenReturn(483L);
         Mockito.when(drTestDiskDao.listActiveBySessionId(1L)).thenReturn(Collections.singletonList(disk));
@@ -196,6 +197,7 @@ public class DrTargetMaterializationServiceImplTest {
         Mockito.when(drPlanDao.findById(planId)).thenReturn(plan);
         Mockito.when(accountDao.findById(1L)).thenReturn(owner);
         Mockito.when(disk.getId()).thenReturn(4L);
+        Mockito.when(disk.getProvider()).thenReturn("FILE");
         Mockito.when(disk.getArtifactRef()).thenReturn(
                 "/run/ablestack-vm-ftctl/dr-runtime/plans/plan/test-sessions/run-artifacts/test-disk.qcow2");
         Mockito.when(disk.getTargetVolumeId()).thenReturn(484L);
